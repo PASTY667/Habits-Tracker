5dd27d762e46dec504ff77ae995dfbf14e527aab
PASTY667 <105501129+PASTY667@users.noreply.github.com>
2023-06-02 21:43:11 +0200

    Update habits.c

---
+++ src/habits.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "habits.h"
+
+void creerHabitude(ListeHabits *liste) {
+    // Code pour créer une nouvelle habitude
+}
+
+void afficherHabitude(const Habit *habitude) {
+    // Code pour afficher une habitude
+}
+
+void modifierHabitude(ListeHabits *liste) {
+    // Code pour modifier une habitude existante
+}
+
+void supprimerHabitude(ListeHabits *liste) {
+    // Code pour supprimer une habitude de la liste
+}
